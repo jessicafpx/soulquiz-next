02516cea6c209686bf82ed35844e89c612b73326
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-27 23:47:07 -0300

    style: add quiz container component

---
+++ src/components/QuizContainer/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const QuizContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 5%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default QuizContainer;
